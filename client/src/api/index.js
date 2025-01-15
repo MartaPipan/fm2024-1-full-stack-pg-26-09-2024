a34c4d48c9f741026157ce117c7b1ddf111441da
Marta Prystatska <tradlisboa@gmail.com>
2025-01-15 19:22:03 +0000

    1. npm install query-string; 2.use query-string in getAllUsers(return httpClient.get(`/users?${queryString.stringify(resultOptions)})

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,24 @@
 import axios from 'axios';
+import queryString from 'query-string';
 
 const httpClient = axios.create({
     baseURL: 'http://localhost:3000'
 })
 
-export const getAllUsers= (page, amount) => httpClient.get (`http://localhost:3000/users?page=${page}&amount=${amount}`); 
+export const getAllUsers = (options = {}) => {
+    const defaultOptions = {
+        page: 1,
+        amount: 10
+    };
+    const resultOptions = {
+        ...defaultOptions, ...options
+    };
+
+    const { page, amount } = resultOptions;
+    if(page && amount){
+        return httpClient.get(`/users?${queryString.stringify(resultOptions)}`)  
+    }
+}; 
+
+
 
